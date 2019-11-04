2c28eafbf08799ea7262df3169b226ebc1930bce
ML <marie.laurent.00@icloud.com>
2019-11-04 16:58:36 +0100

    improve css

--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { removeFromCart } from '../actions/actionsCart';
+import { Table } from 'reactstrap';
 
 const Cart = () => {
   const dispatch = useDispatch();
@@ -11,9 +12,17 @@ const Cart = () => {
 
   const _renderBasket = () => {
     return data.map(item => (
-      <ul key={item.id}>
-        <li>{item.number} - {item.name} - {item.price} € <button onClick={removePanier(item)}>Remove</button> </li>
-      </ul>
+      <tr key={item.id}>
+        <th id="thCenter" scope="row">{item.number}</th>
+        <td id="thCenter"><img alt="product" src={item.img} /></td>
+        <td id="thCenter">{item.name}</td>
+        <td id="thCenter">{item.price} €</td>
+        <td id="thCenter"><button onClick={removePanier(item)}>Remove</button></td>
+      </tr>
+
+      // <ul className="ulTab" key={item.id}>
+      //   <li className="listTab"> <div><img alt="product" src={item.img} /></div> <div className="numberTab">{item.number}</div> <div>{item.name}</div> <div>{item.price} €</div> <div><button onClick={removePanier(item)}>Remove</button></div> </li>
+      // </ul>
     ));
   };
 
@@ -23,10 +32,25 @@ const Cart = () => {
 
   return (
     <div>
-      <span>Votre Panier : </span>
-      {_renderBasket()}
-      <span>Total : </span>
-      {_renderPrice()} €
+      <Table striped responsive>
+        <thead>
+          <tr>
+            <th>Number</th>
+            <th>Picture</th>
+            <th>Name Product</th>
+            <th>Price</th>
+            <th>Remove from Cart</th>
+          </tr>
+        </thead>
+        <tbody>
+          {_renderBasket()}
+          <tr>
+            <td colspan="6" className="alert-link">Total : {_renderPrice()} €</td>
+          </tr>
+        </tbody>
+      </Table>
+      {/* <span>Total : </span>
+      {_renderPrice()} € */}
     </div>
   );
 };
